8621776aa5eb2410ca0fce1eb5b5a64d1bc9d312
Jemara Smith <jemarasmith@gmail.com>
2022-10-23 13:26:02 -0400

    Initial Commit

---
+++ Hair Beau 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hair Beau 2
+//
+//  Created by Jemara Smith on 10/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
